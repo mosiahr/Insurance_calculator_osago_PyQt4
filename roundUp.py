3b9688c7d669b1f8c9566302806b92bb780e5375
Hryhorii Mosia <grishamosya@gmail.com>
2016-10-31 02:17:20 +0200

    Correction osago.py and roundUp.py.Cleaning excess

--- roundUp.py
+++ roundUp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 def roundUp(x, y=None):
@@ -8,7 +9,7 @@ def roundUp(x, y=None):
         if y != None:
             if len(right) > y:
                 if int(right[y]) > 4:
-                    right = right[:y-1] + str(int(right[y-1])+1) 
+                    right = right[:y-1] + str(int(right[y-1])+1)
                 right = right[:y]
             return float(left + '.' + right)
         else:
@@ -17,13 +18,3 @@ def roundUp(x, y=None):
             return float(left)
     else:
         return float(x)
-
-    
-
-
-
-# if __name__ == '__main__':                     
-#     # x = input('Enter the number: ')
-#     # y = input('Enter the rank: ')
-#     # print 'You enter the number: {}  \nYou enter the rank: {}'.format(x,y) 
-#     print roundUp(x, y)
